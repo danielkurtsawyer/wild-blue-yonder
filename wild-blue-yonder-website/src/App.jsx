cfc58be957a57b4d03d367be5156871965b5182c
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-04-19 12:23:57 -0400

    Updated title and favicon

--- wild-blue-yonder-website/src/App.jsx
+++ wild-blue-yonder-website/src/App.jsx
@@ -6,19 +6,11 @@ import './App.css'
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header></header>
+      <main>
+        Wild Blue Yonder Website: It All Starts Here
+      </main>
+      <footer></footer>
     </>
   )
 }
