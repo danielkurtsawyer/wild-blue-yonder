211387e599559add97bd9f541bb260e6511d3528
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-04-19 12:50:01 -0400

    Set up prettier configuration with ESLint and define custom font families

--- wild-blue-yonder-website/src/App.jsx
+++ wild-blue-yonder-website/src/App.jsx
@@ -8,7 +8,8 @@ function App() {
     <>
       <header></header>
       <main>
-        Wild Blue Yonder Website: It All Starts Here
+        <div className="heading-text">Wild Blue Yonder Website: It All Starts Here</div>
+        <div className="body-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In accumsan elit in leo semper, non rutrum tortor faucibus. Aliquam at porttitor enim, ut euismod orci. Pellentesque sagittis placerat est ac tempor. Proin in dui auctor lorem molestie congue. Sed commodo, mi a pulvinar ullamcorper, nibh metus pellentesque dui, vitae mollis urna nibh quis dolor. Proin dapibus tincidunt malesuada. Nunc tempus, lacus id malesuada vulputate, lacus ante volutpat purus, sed eleifend lectus tortor nec ante. In lacinia massa nec blandit interdum. Ut tristique consectetur nisl ut auctor. Mauris eu odio id lorem pellentesque euismod. Nulla pretium lorem lorem, quis tincidunt mi feugiat quis. Nulla ultrices nibh et tellus rutrum malesuada. Nunc in risus dictum, molestie tortor vitae, vehicula eros.</div>
       </main>
       <footer></footer>
     </>
