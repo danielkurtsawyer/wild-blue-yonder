9eb1879a0837a3d5c1a9d19fc2c6274fee5d1d73
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-04-21 15:48:06 -0400

    Clear main div content for next component creation

--- wild-blue-yonder-website/src/App.jsx
+++ wild-blue-yonder-website/src/App.jsx
@@ -8,6 +8,7 @@ import ButtonTertiary from "./components/Button/ButtonTertiary.jsx";
 import LinkPrimary from "./components/Link/LinkPrimary.jsx";
 import LinkSecondary from "./components/Link/LinkSecondary.jsx";
 import LinkTertiary from "./components/Link/LinkTertiary.jsx";
+import UpcomingShows from "./components/UpcomingShows/UpcomingShows.jsx";
 
 function App() {
   return (
@@ -15,38 +16,7 @@ function App() {
       <header className="shadow-gradient-top-elevation-low">
         <Header />
       </header>
-      <main>
-        <ButtonPrimary
-          href="https://www.google.com/"
-          text="Stream Bridges"
-          className="shadow-gradient-bottom-elevation-medium"
-        />
-        <ButtonSecondary
-          href="https://www.google.com/"
-          text="Watch the Music Video"
-          className="shadow-gradient-bottom-elevation-medium"
-        />
-        <ButtonTertiary
-          href="https://www.google.com/"
-          text="Watch the Live Version"
-          className="shadow-gradient-bottom-elevation-medium"
-        />
-        <LinkPrimary
-          href="https://www.google.com/"
-          text="Primary"
-          className="link-primary"
-        />
-        <LinkSecondary
-          href="https://www.google.com/"
-          text="Secondary"
-          className="link-secondary"
-        />
-        <LinkTertiary
-          href="https://www.google.com/"
-          text="Tertiary"
-          className="link-tertiary"
-        />
-      </main>
+      <main></main>
       <footer>
         <Footer />
       </footer>
