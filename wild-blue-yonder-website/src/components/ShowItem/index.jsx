912b69722b281918d0801290adc1cb590c147b94
Jason Kuehnle <99767473+jasonkuehnle@users.noreply.github.com>
2025-04-21 14:36:21 -0400

    First show item has a border-top

--- wild-blue-yonder-website/src/components/ShowItem/index.jsx
+++ wild-blue-yonder-website/src/components/ShowItem/index.jsx
@@ -1,9 +1,16 @@
 import "./styles.css";
 import ButtonPrimary from "../Button/ButtonPrimary.jsx";
 
-export default function ShowItem({ showDate, venueName, venueLocation }) {
+export default function ShowItem({
+  showDate,
+  venueName,
+  venueLocation,
+  borderTop,
+}) {
+  console.log("showDate (index):", showDate); // See if 0 shows up
+
   return (
-    <div className="show-item-container">
+    <div className="show-item-container" style={{ borderTop }}>
       <h4>{showDate ? showDate : "Show Date"}</h4>
       <p className="large-text">{venueName ? venueName : "Venue Name"}</p>
       <p className="large-text">
--- wild-blue-yonder-website/src/components/UpcomingShows/index.jsx
+++ wild-blue-yonder-website/src/components/UpcomingShows/index.jsx
@@ -6,15 +6,18 @@ export default function UpcomingShows() {
   return (
     <div className="shows-container">
       <h2 className="upcoming-shows-header">Upcoming Shows</h2>
-      {shows.map((show, index) => (
-        <ShowItem
-          key={index}
-          showDate={show.date}
-          venueName={show.name}
-          venueLocation={show.location}
-          borderTop={`${index === 0 ? "1px solid black" : ""}`}
-        />
-      ))}
+      {shows.map((show, index) => {
+        console.log("Rendering index:", index);
+        return (
+          <ShowItem
+            key={index}
+            showDate={show.date}
+            venueName={show.name}
+            venueLocation={index === 0 ? "hi" : "no"}
+            borderTop={index === 0 ? "2px solid black" : ""}
+          />
+        );
+      })}
       {/* <ShowItem
         showDate="August 16, 2025"
         venueName="Yonder Fest"
