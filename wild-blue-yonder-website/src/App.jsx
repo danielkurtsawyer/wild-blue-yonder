b6ed398723eeff1b93fa6da4e9bc2d354fcc6ba9
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-04-23 18:50:51 -0400

    Remove unneeded component imports from App.jsx

--- wild-blue-yonder-website/src/App.jsx
+++ wild-blue-yonder-website/src/App.jsx
@@ -6,15 +6,6 @@ import Music from "./pages/Music.jsx";
 
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
-import ButtonPrimary from "./components/Button/ButtonPrimary.jsx";
-import ButtonSecondary from "./components/Button/ButtonSecondary.jsx";
-import ButtonTertiary from "./components/Button/ButtonTertiary.jsx";
-import LinkPrimary from "./components/Link/LinkPrimary.jsx";
-import LinkSecondary from "./components/Link/LinkSecondary.jsx";
-import LinkTertiary from "./components/Link/LinkTertiary.jsx";
-import UpcomingShows from "./components/UpcomingShows/UpcomingShows.jsx";
-import HeroImage from "./components/HeroImage/HeroImage.jsx";
-import FeaturedRelease from "./components/FeaturedRelease/FeaturedRelease.jsx";
 
 function App() {
   return (
