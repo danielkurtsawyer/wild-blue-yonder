239888b3d567bbde0ff410696bd156f549777061
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-04-24 13:31:02 -0400

    Finish about-page
    I removed the AboutUs component and threw everything into the About.jsx page. Since the AboutUs component was not reusable, it didn't make sense being its own component. I also fixed the text alignment of each section. Press coverage section may or may not get added at a later date.

--- wild-blue-yonder-website/src/pages/About/About.jsx
+++ wild-blue-yonder-website/src/pages/About/About.jsx
@@ -1,10 +1,61 @@
-import AboutUs from "../../components/AboutUs/AboutUs";
+import "./about.css";
+import oakland from "../../assets/images/Photos/oakland.jpg";
+import thunderbirdOverhead from "../../assets/images/Photos/thunderbird overhead.jpg";
+import thunderbirdFloor from "../../assets/images/Photos/thunderbird floor.jpg";
+import LinkPrimary from "../../components/Link/LinkPrimary";
 
 function About() {
   return (
-    <>
-      <AboutUs />
-    </>
+    <div className="about-container">
+      <h1>About Us</h1>
+      <div className="content-sections">
+        <div className="pic-and-paragraph">
+          <img
+            className="shadow-gradient-bottom-elevation-medium"
+            src={oakland}
+            alt="Oakland"
+          />
+          <p className="band-members">
+            <b>Frank Mileto</b> (Aux Percussion), <b>Mark Riggio</b> (Drums),{" "}
+            <b>Dan Sawyer</b> (Bass/Guitar/Vocals), <b>Jason Kuehnle</b> (Keys),{" "}
+            <b>Eli Alfieri</b> (Guitar/Bass/Vocals), and <b>Lucas Sherman</b>{" "}
+            (Saxophone)
+          </p>
+        </div>
+        <div className="pic-and-paragraph">
+          <h2>Biography</h2>
+          <img
+            className="shadow-gradient-bottom-elevation-medium"
+            src={thunderbirdFloor}
+            alt="ThunderBirdFloor"
+          />
+          <p>
+            Coming from Pittsburgh, PA, Wild Blue Yonder draws inspiration from
+            funk, jazz, R&B/soul, and folk music, creating music that flows like
+            rivers and blossoms like wildflowers. Their style mixes deliberate
+            songwriting and spontaneous improvisation, making each song into an
+            organic voyage of growth and exploration.
+          </p>
+        </div>
+        <div className="pic-and-paragraph">
+          <h2>Book Us</h2>
+          <img
+            className="shadow-gradient-bottom-elevation-medium"
+            src={thunderbirdOverhead}
+            alt="ThunderBirdOverhead"
+          />
+          <p className="booking-text">
+            Interested in booking Wild Blue Yonder? Booking inquiries can be
+            sent to: <br /> <br />
+            <LinkPrimary
+              href="https://google.com"
+              text="booking@wildblueyondermusic.com"
+              // className="shadow-gradient-bottom-elevation-medium"
+            />
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
 
