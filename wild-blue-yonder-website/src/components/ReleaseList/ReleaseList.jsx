644fd287118b2bbbd83c6c459e66796e5ec726e8
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-04-25 11:21:43 -0400

    Refactor ReleaseList to dynamically render based on array object input

--- wild-blue-yonder-website/src/components/ReleaseList/ReleaseList.jsx
+++ wild-blue-yonder-website/src/components/ReleaseList/ReleaseList.jsx
@@ -1,30 +1,43 @@
 import ReleaseItem from "../ReleaseItem/ReleaseItem";
 import "./release-list.css";
 
-// add new releases here
+// import new releases here
 import BridgesCoverArt from "../../assets/images/Album Art/bridges cover.jpg";
 import JoleneCoverArt from "../../assets/images/Album Art/Jolene cover.jpeg";
 import WhenImWithYouCoverArt from "../../assets/images/Album Art/when im with you cover.jpeg";
 import GreenvilleCoverArt from "../../assets/images/Album Art/live from green cover.jpeg";
 import DancingAstrayCoverArt from "../../assets/images/Album Art/dancing astray cover.jpg";
 
+const releases = [
+  // add new releases here
+  {
+    src: BridgesCoverArt,
+    name: "Bridges",
+  },
+  {
+    src: JoleneCoverArt,
+    name: "Jolene",
+  },
+  {
+    src: WhenImWithYouCoverArt,
+    name: "When I'm With You",
+  },
+  {
+    src: GreenvilleCoverArt,
+    name: "Greenville, SC 6/17/23 (Live)",
+  },
+  {
+    src: DancingAstrayCoverArt,
+    name: "Dancing Astray",
+  },
+];
+
 function ReleaseList() {
   return (
     <div className="release-list-container">
-      <ReleaseItem coverArt={BridgesCoverArt} releaseName="Bridges" />
-      <ReleaseItem coverArt={JoleneCoverArt} releaseName="Jolene" />
-      <ReleaseItem
-        coverArt={WhenImWithYouCoverArt}
-        releaseName="When I'm With You"
-      />
-      <ReleaseItem
-        coverArt={GreenvilleCoverArt}
-        releaseName="Greenville, SC 6/17/23 (Live)"
-      />
-      <ReleaseItem
-        coverArt={DancingAstrayCoverArt}
-        releaseName="Dancing Astray"
-      />
+      {releases.map((release) => (
+        <ReleaseItem coverArt={release.src} releaseName={release.name} />
+      ))}
     </div>
   );
 }
