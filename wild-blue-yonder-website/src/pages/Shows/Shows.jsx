92a08ddacce24ff2bb6b551c69b7fa348b159d3f
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-04-27 12:35:44 -0400

    Add poker graphic on Shows page

--- wild-blue-yonder-website/src/pages/Shows/Shows.jsx
+++ wild-blue-yonder-website/src/pages/Shows/Shows.jsx
@@ -1,10 +1,12 @@
 import UpcomingShows from "../../components/UpcomingShows/UpcomingShows";
+import pokerGraphic from "../../assets/images/Graphics/poker.svg";
 import "./shows.css";
 
 function Shows() {
   return (
     <div className="shows-page-container">
       <UpcomingShows />
+      <img className="poker" src={pokerGraphic} alt="" />
     </div>
   );
 }
