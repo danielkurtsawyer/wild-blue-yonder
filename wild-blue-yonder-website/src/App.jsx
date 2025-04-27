865aa10867bd1836e7c9fca71ce995fd3f69b8cb
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-04-27 14:37:08 -0400

    Fix import typo

--- wild-blue-yonder-website/src/App.jsx
+++ wild-blue-yonder-website/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home/Home.jsx";
 import Shows from "./pages/Shows/Shows.jsx";
 import Videos from "./pages/Videos.jsx";
 import Music from "./pages/Music/Music.jsx";
-import About from "./pages/About/about.jsx";
+import About from "./pages/About/About.jsx";
 
 import Header from "./components/Header/Header.jsx";
 import Footer from "./components/Footer/Footer.jsx";
