5205dead32ff43d17a5b43e83cb49b4db379ce23
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-05-04 12:50:13 -0400

    Update upcoming shows data

--- wild-blue-yonder-website/src/components/UpcomingShows/shows.js
+++ wild-blue-yonder-website/src/components/UpcomingShows/shows.js
@@ -1,21 +1,21 @@
 const shows = [
   {
-    date: "August 15, 2025",
-    name: "Yonder Fest",
-    location: "ButtFuck, PA",
+    date: "May 31, 2025",
+    name: "Funky Fest",
+    location: "The Glades festival Grounds",
     ticketLink: "https://google.com",
   },
   {
-    date: "May 1, 2025",
-    name: "Bottlerocket Music Hall",
+    date: "June 14, 2025",
+    name: "Parking Pad",
+    location: "Dormont, PA",
+    ticketLink: "https://google.com",
+  },
+  {
+    date: "July 24, 2025",
+    name: "Haven",
     location: "Pittsburgh, PA",
-    title: "Supporting Consider the Source",
-    ticketLink: "https://google.com",
-  },
-  {
-    date: "December 31, 2025",
-    name: "123 Pleasant",
-    location: "Morgantown, WV",
+    title: "Phish After Party",
     ticketLink: "https://google.com",
   },
 ];
