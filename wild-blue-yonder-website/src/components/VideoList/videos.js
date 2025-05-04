daf7967f0e94bfa99f494a602a357fd53243ebcb
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-05-04 12:58:15 -0400

    Update video list

--- wild-blue-yonder-website/src/components/VideoList/videos.js
+++ wild-blue-yonder-website/src/components/VideoList/videos.js
@@ -1,20 +1,42 @@
 const videos = [
-    {
-        title: "Wild Blue Yonder - Bridges | Official Music Video",
-        link: "https://youtu.be/d_t6Ajy9c9I?si=s8lV9JGiPjNqVFDn",
-    },
-    {
-        title: "Wild Blue Yonder - Jolene | Official Music Video",
-        link: "https://youtu.be/_FKBYCmBBLU?si=UR7b9kGh4XBWe2-M",
-    },
-    {
-        title: "Wild Blue Yonder - Pink Pony Club Cover",
-        link: "https://youtu.be/hBtDP_Q-X5g?si=dfNhNuAj02uRV8Zq",
-    },
-    {
-        title: "Wild Blue Yonder - Cut The Rope. Live From Post Genre: Genesis III 3/1/24",
-        link: "https://youtu.be/hkJaVGV-9Ts?si=bFuHypszG6yljQVI",
-    },
+  {
+    title: "Wild Blue Yonder - Bridges | Official Music Video",
+    link: "https://youtu.be/d_t6Ajy9c9I?si=s8lV9JGiPjNqVFDn",
+  },
+  {
+    title: "Wild Blue Yonder - Jolene | Official Music Video",
+    link: "https://youtu.be/_FKBYCmBBLU?si=UR7b9kGh4XBWe2-M",
+  },
+  {
+    title: "Wild Blue Yonder - Pink Pony Club Cover",
+    link: "https://youtu.be/hBtDP_Q-X5g?si=dfNhNuAj02uRV8Zq",
+  },
+  {
+    title:
+      "Wild Blue Yonder - Cut The Rope. Live From Post Genre: Genesis III 3/1/24",
+    link: "https://youtu.be/hkJaVGV-9Ts?si=bFuHypszG6yljQVI",
+  },
+  {
+    title:
+      "Wild Blue Yonder - The Band Played On | Live from Club Cafe 6/29/23",
+    link: "https://youtu.be/ARyP_qklMC8?si=wtyi5QUEm0g7RChY",
+  },
+  {
+    title: "Wild Blue Yonder - Sun Jam | Live From Greenville 6/17/23",
+    link: "https://youtu.be/rrvyTOUDjkY?si=pGRF1toTgFuP7KKz",
+  },
+  {
+    title: "When I'm With You - Wild Blue Yonder | Lyric Video",
+    link: "https://youtu.be/nc3-Q7REdtc?si=GsAR6hhIbx4-iQze",
+  },
+  {
+    title: "Where's the Album",
+    link: "https://youtu.be/ZofRVACJegM?si=RP8maZeXDndiG6cF",
+  },
+  {
+    title: "Wild Blue Yonder - Angels Prevail",
+    link: "https://youtu.be/J-lxI9avcWY?si=0Jdjf_N0VvhIkqyZ",
+  },
 ];
 
 export default videos;
