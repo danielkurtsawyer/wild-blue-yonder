3c513c41c03dfc32012ddf9c754228aca89e34ea
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-05-07 15:07:44 -0400

    Add properties to FeaturedRelease component in Home page

--- wild-blue-yonder-website/src/pages/Home/Home.jsx
+++ wild-blue-yonder-website/src/pages/Home/Home.jsx
@@ -1,13 +1,24 @@
 import HeroImage from "../../components/HeroImage/HeroImage";
 import FeaturedRelease from "../../components/FeaturedRelease/FeaturedRelease";
 import UpcomingShows from "../../components/UpcomingShows/UpcomingShows";
+import BridgesArt from "../../assets/images/Album Art/bridges cover.jpg";
+
 import "./home.css";
 
 function Home() {
   return (
     <div className="home-page-container">
       <HeroImage />
-      <FeaturedRelease />
+      <FeaturedRelease
+        src={BridgesArt}
+        alt=""
+        heading="Bridges: Out Now!"
+        description="Stream the newest release from Wild Blue Yonder!"
+        primaryLink="https://distrokid.com/hyperfollow/wildblueyonder/bridges"
+        primaryText="Stream Bridges"
+        secondaryLink="https://youtu.be/d_t6Ajy9c9I?si=eQs55cBzLSrjnoQa"
+        secondaryText="Watch the Music Video"
+      />
       <UpcomingShows />
     </div>
   );
