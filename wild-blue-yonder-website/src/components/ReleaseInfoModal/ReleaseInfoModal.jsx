087de6781f5fa7446a5caad51ab253a6b2565c95
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-05-14 16:21:02 -0400

    Add animation to Listen streaming link icons

--- wild-blue-yonder-website/src/components/ReleaseInfoModal/ReleaseInfoModal.jsx
+++ wild-blue-yonder-website/src/components/ReleaseInfoModal/ReleaseInfoModal.jsx
@@ -22,6 +22,39 @@ function ReleaseInfoModal({
     modal.classList.remove("active");
     document.body.style.overflow = "visible";
   }
+  function handleHover(index) {
+    let allATags = document.querySelectorAll(
+      ".release-info-modal-links-container a"
+    );
+    allATags.forEach((tag) => {
+      let indexOfTag = +tag.getAttribute("index");
+      if (indexOfTag === index) {
+        tag.classList.add("active");
+        if (tag.classList.contains("inactive")) {
+          tag.classList.remove("inactive");
+        }
+      } else {
+        if (tag.classList.contains("active")) {
+          tag.classList.remove("active");
+        }
+        tag.classList.add("inactive");
+      }
+    });
+  }
+
+  function handleLeave() {
+    let allATags = document.querySelectorAll(
+      ".release-info-modal-links-container a"
+    );
+    allATags.forEach((tag) => {
+      if (tag.classList.contains("active")) {
+        tag.classList.remove("active");
+      }
+      if (tag.classList.contains("inactive")) {
+        tag.classList.remove("inactive");
+      }
+    });
+  }
   return (
     <div className="release-info-modal-container">
       <div className="release-info-modal shadow-elevation-highest">
@@ -45,13 +78,34 @@ function ReleaseInfoModal({
         <div className="release-info-modal-listen">
           <p className="large-text">Listen</p>
           <div className="release-info-modal-links-container">
-            <a href={spotify} target="_blank" rel="noopener noreferrer">
+            <a
+              href={spotify}
+              target="_blank"
+              rel="noopener noreferrer"
+              index="0"
+              onMouseEnter={() => handleHover(0)}
+              onMouseLeave={() => handleLeave()}
+            >
               <img src={spotifyLogo} alt="Spotify" />
             </a>
-            <a href={appleMusic} target="_blank" rel="noopener noreferrer">
+            <a
+              href={appleMusic}
+              target="_blank"
+              rel="noopener noreferrer"
+              index="1"
+              onMouseEnter={() => handleHover(1)}
+              onMouseLeave={() => handleLeave()}
+            >
               <img src={appleMusicLogo} alt="Apple Music" />
             </a>
-            <a href={youtube} target="_blank" rel="noopener noreferrer">
+            <a
+              href={youtube}
+              target="_blank"
+              rel="noopener noreferrer"
+              index="2"
+              onMouseEnter={() => handleHover(2)}
+              onMouseLeave={() => handleLeave()}
+            >
               <img src={youtubeLogo} alt="Youtube" />
             </a>
           </div>
