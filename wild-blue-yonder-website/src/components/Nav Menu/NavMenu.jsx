4182aa5442253fc088a832dace355717b62ba4b5
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-05-14 14:39:19 -0400

    Added animations to the nav menu
    Followed a similar approach as the previous two commits, except there is no scale transform,
    only an opacity transition to emphasize the hovered navLink by de-emphasizing the other links.

--- wild-blue-yonder-website/src/components/Nav Menu/NavMenu.jsx
+++ wild-blue-yonder-website/src/components/Nav Menu/NavMenu.jsx
@@ -2,12 +2,44 @@ import { NavLink, Link } from "react-router";
 import "./nav-menu.css";
 
 function NavMenu({ onClick }) {
+  function handleHover(index) {
+    let allATags = document.querySelectorAll(".nav-menu a");
+    allATags.forEach((tag) => {
+      let indexOfTag = +tag.getAttribute("index");
+      if (indexOfTag === index) {
+        tag.classList.add("hover");
+        if (tag.classList.contains("inactive")) {
+          tag.classList.remove("inactive");
+        }
+      } else {
+        if (tag.classList.contains("hover")) {
+          tag.classList.remove("hover");
+        }
+        tag.classList.add("inactive");
+      }
+    });
+  }
+
+  function handleLeave() {
+    let allATags = document.querySelectorAll(".nav-menu a");
+    allATags.forEach((tag) => {
+      if (tag.classList.contains("hover")) {
+        tag.classList.remove("hover");
+      }
+      if (tag.classList.contains("inactive")) {
+        tag.classList.remove("inactive");
+      }
+    });
+  }
   return (
     <nav className="nav-menu">
       <NavLink
         to="/"
         className={({ isActive }) => (isActive ? "active" : "")}
         onClick={onClick}
+        onMouseEnter={() => handleHover(0)}
+        onMouseLeave={() => handleLeave()}
+        index="0"
       >
         Home
       </NavLink>
@@ -15,6 +47,9 @@ function NavMenu({ onClick }) {
         to="/shows"
         className={({ isActive }) => (isActive ? "active" : "")}
         onClick={onClick}
+        onMouseEnter={() => handleHover(1)}
+        onMouseLeave={() => handleLeave()}
+        index="1"
       >
         Shows
       </NavLink>
@@ -22,6 +57,9 @@ function NavMenu({ onClick }) {
         to="/music"
         className={({ isActive }) => (isActive ? "active" : "")}
         onClick={onClick}
+        onMouseEnter={() => handleHover(2)}
+        onMouseLeave={() => handleLeave()}
+        index="2"
       >
         Music
       </NavLink>
@@ -29,6 +67,9 @@ function NavMenu({ onClick }) {
         to="/videos"
         className={({ isActive }) => (isActive ? "active" : "")}
         onClick={onClick}
+        onMouseEnter={() => handleHover(3)}
+        onMouseLeave={() => handleLeave()}
+        index="3"
       >
         Videos
       </NavLink>
@@ -36,6 +77,9 @@ function NavMenu({ onClick }) {
         to="/about"
         className={({ isActive }) => (isActive ? "active" : "")}
         onClick={onClick}
+        onMouseEnter={() => handleHover(4)}
+        onMouseLeave={() => handleLeave()}
+        index="4"
       >
         About
       </NavLink>
