8b31bce4644fe1a7f138adff4b8e94976274e15c
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-06-15 12:36:01 -0400

    Update shows list

--- wild-blue-yonder-website/src/components/UpcomingShows/shows.js
+++ wild-blue-yonder-website/src/components/UpcomingShows/shows.js
@@ -1,12 +1,5 @@
 const shows = [
   {
-    date: "June 14, 2025",
-    name: "The Parking Pad",
-    location: "Dormont, PA",
-    ticketLink:
-      "https://www.eventbrite.com/e/wild-blue-yonder-live-from-the-parking-pad-tickets-1247157273979?aff=ebdsshios",
-  },
-  {
     date: "June 24, 2025",
     name: "Haven",
     location: "Pittsburgh, PA",
