dc44e25bc98b9ca80d03bbbb34a11b38232e0cbe
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-06-25 13:30:18 -0400

    Update show list

--- wild-blue-yonder-website/src/components/UpcomingShows/shows.js
+++ wild-blue-yonder-website/src/components/UpcomingShows/shows.js
@@ -1,12 +1,5 @@
 const shows = [
   {
-    date: "June 24, 2025",
-    name: "Haven",
-    location: "Pittsburgh, PA",
-    title: "Phish After Show with Satellite Dog",
-    ticketLink: "https://indietix.org/events/postgenre/06242025",
-  },
-  {
     date: "August 23, 2025",
     name: "Grandview Park",
     location: "Pittsburgh, PA",
