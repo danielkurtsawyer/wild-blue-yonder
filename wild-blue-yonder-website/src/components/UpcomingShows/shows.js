945ee1590c15843e166414b21917c64a97df7da8
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-08-28 14:21:45 -0400

    Remove old show

--- wild-blue-yonder-website/src/components/UpcomingShows/shows.js
+++ wild-blue-yonder-website/src/components/UpcomingShows/shows.js
@@ -1,14 +1,9 @@
 const shows = [
   {
-    date: "August 23, 2025",
-    name: "Grandview Park",
-    location: "Pittsburgh, PA",
-  },
-  {
     date: "September 27, 2025",
     name: "Bellevue Music Festival",
-    location: "Bellevue, PA"
-  }
+    location: "Bellevue, PA",
+  },
 ];
 
 export default shows;
