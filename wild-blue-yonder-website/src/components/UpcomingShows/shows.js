9a33a923bf5fc65a5e4b2aa7b2d993718a6b34c5
danielkurtsawyer <[19danielsawyer@gmail.com]>
2025-08-28 14:29:27 -0400

    Add new show

--- wild-blue-yonder-website/src/components/UpcomingShows/shows.js
+++ wild-blue-yonder-website/src/components/UpcomingShows/shows.js
@@ -1,5 +1,11 @@
 const shows = [
   {
+    date: "September 20, 2025",
+    title: "Pistons in the Park",
+    name: "Allegheny Commons Park West",
+    location: "Pittsburgh, PA",
+  },
+  {
     date: "September 27, 2025",
     name: "Bellevue Music Festival",
     location: "Bellevue, PA",
